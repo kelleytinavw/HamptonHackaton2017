84474e563139d67c9ce3ae8bb0c677542491ae78
kelleytinavw <kelleytina.williams@my.hamptonu.edu>
2017-10-14 15:50:02 -0400

    bricks delete finished

--- core/src/com/hampton/game/demo/NinjaBounce.java
+++ core/src/com/hampton/game/demo/NinjaBounce.java
@@ -18,12 +18,11 @@ import com.hampton.game.utils.ActorUtils;
  */
 public class NinjaBounce extends GameScreen {
 
-    //private Actor ninja;
     private float xMove;
     private float yMove;
     private final float MAX_MOVE = 10;
     private Actor ball;
-    private Actor [] bricks = new Actor[25];
+    private Actor [] bricks = new Actor[20];
     private Actor bar;
 
     //private float moveBar;
@@ -112,8 +111,8 @@ public class NinjaBounce extends GameScreen {
         if (ActorUtils.actorsCollided(bar,ball)){
             yMove = Math.abs(yMove);
         }
-        if (ActorUtils.actorsCollided(bricks[], ball)){
-            for(int i = 0; i < bricks.length; i++) {
+        for(int i = 0; i < bricks.length; i++){
+            if (ActorUtils.actorsCollided(bricks[i], ball)){
                 bricks[i].remove();
             }
         }
